0f282df1f455e85cf2ab68eed5ed5fa9893ce0a2
Nwakaego Glory Nwaekpe <glorynwaekpe@gmail.com>
2023-03-20 21:37:05 +0100

    work on the main page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,29 @@ function App() {
 
   return (
     <div className="App">
-      <p>Hello</p>
+      <div className="navbar">
+        <h3>Diary</h3>
+        <div className="navbtn">
+          <button className="signout">Sign out</button>
+          <button className="btn-jo">JO</button>
+        </div>
+      </div>
+      <div className="mid-section">
+        <input type="text" id="name" className="input-text" />
+        <div class="mid-section-bottom">
+          <p>1/300</p>
+          <button class="btn-save">save</button>
+        </div>
+      </div>
+      <div className="bottom">
+        <button className="btn-jo">JO</button>
+        <small className="name">Jibola Orija</small>
+        <p className="note">I am tired, I really need a vacation</p>
+        <div className="delete-update-btn">
+          <button className="delete">Delete</button>
+          <button className="update">Update</button>
+        </div>
+      </div>
     </div>
   );
 }
